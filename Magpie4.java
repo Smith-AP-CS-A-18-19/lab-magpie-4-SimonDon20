20af642af5d217a009a92dae00b40eca44e559be
Donovan <SimonDon@berkeleyprep.org>
2018-10-25 23:49:45 -0400

    Took me long enough lol

--- Magpie4.java
+++ Magpie4.java
@@ -97,8 +97,101 @@ public class Magpie4 {
 			statement = statement.substring (0, statement.length() - 1);
 		}
 		int psnYou = findKeyword (statement, "you", 0);
-		int psnMe = findKeyword (statement, "me", psnOfYou + 3);
+		int psnMe = findKeyword (statement, "me", psnYou + 3);
 		String restOfStatement = statement.substring(psnYou + 3, psnMe).trim();
 		return "What makes you think that I  " + restOfStatement + " you?";
 	}
+
+	private int findKeyword(String statement, String goal,
+			int startPos)
+	{
+		String phrase = statement.trim().toLowerCase();
+		goal = goal.toLowerCase();
+
+		// The only change to incorporate the startPos is in
+		// the line below
+		int psn = phrase.indexOf(goal, startPos);
+
+		// Refinement--make sure the goal isn't part of a
+		// word
+		while (psn >= 0) {
+			// Find the string of length 1 before and after
+			// the word
+			String before = " ", after = " ";
+			if (psn > 0) {
+				before = phrase.substring(psn - 1, psn);
+			} if (psn + goal.length() < phrase.length()) {
+				after = phrase.substring(
+						psn + goal.length(),
+						psn + goal.length() + 1);
+			}
+
+			// If before and after aren't letters, we've
+			// found the word
+			if (((before.compareTo("a") < 0) || (before
+					.compareTo("z") > 0)) // before is not a
+											// letter
+					&& ((after.compareTo("a") < 0) || (after
+							.compareTo("z") > 0)))
+			{
+				return psn;
+			}
+
+			// The last position didn't work, so let's find
+			// the next, if there is one.
+			psn = phrase.indexOf(goal, psn + 1);
+
+		}
+
+		return -1;
+	}
+
+	/**
+	 * Search for one word in phrase. The search is not case
+	 * sensitive. This method will check that the given goal
+	 * is not a substring of a longer string (so, for
+	 * example, "I know" does not contain "no"). The search
+	 * begins at the beginning of the string.
+	 *
+	 * @param statement
+	 *            the string to search
+	 * @param goal
+	 *            the string to search for
+	 * @return the index of the first occurrence of goal in
+	 *         statement or -1 if it's not found
+	 */
+	private int findKeyword(String statement, String goal) {
+		return findKeyword(statement, goal, 0);
+	}
+
+	/**
+	 * Pick a default response to use if nothing else fits.
+	 *
+	 * @return a non-committal string
+	 */
+	private String getRandomResponse() {
+		final int NUMBER_OF_RESPONSES = 4;
+		double r = Math.random();
+		int whichResponse = (int) (r * NUMBER_OF_RESPONSES);
+		String response = "";
+
+		if (whichResponse == 0) {
+			response = "Interesting, tell me more.";
+		} else if (whichResponse == 1) {
+			response = "Hmmm.";
+		} else if (whichResponse == 2) {
+			response = "Do you really think so?";
+		} else if (whichResponse == 3) {
+			response = "You don't say.";
+		} else if (whichResponse == 4)
+		{
+			response = "Input invalid. Please try again.";
+		} else if (whichResponse == 5)
+		{
+			response = "Should I be concerned?";
+		}
+
+		return response;
+	}
+
 }
